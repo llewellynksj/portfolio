4d94d80b46c4a20e8f4ede09ce7cf62a020398db
Kate L <llewellyn.ksj@gmail.com>
2024-05-07 12:58:24 +0100

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+import NavBar from './components/NavBar';
+
+
+function App() {
+
+  return (
+    <>
+    <div>
+      <NavBar />
+    </div>
+      
+      <h1>Hello</h1>
+    </>
+  )
+}
+
+export default App
